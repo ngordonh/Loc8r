6358cc4922451f00eaf63c6fa8b0b085b6d1270c
Nolan Gordon-Hamm <ngordonh@gmail.com>
2016-04-20 15:00:36 -0400

    Added mockup About page

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -8,11 +8,17 @@ module.exports.locationInfo = function (req,res) {
     res.render('location-info', {title: 'Locations Info'});
 };
 
-/* Get 'Add review' page */
+/* GET 'Add review' page */
 module.exports.addReview = function (req,res) {
     res.render('index', {title: 'Add review'});
 };
 
+/* GET 'Add review forum' */
 module.exports.addReview = function(req, res) {
     res.render('location-review-form', { title: 'Add review'});
+};
+
+/* GET 'About page' */
+module.exports.about = function(req, res) {
+    res.render('generic-text', {title: 'About'});
 };
